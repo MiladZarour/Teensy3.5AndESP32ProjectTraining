aa09a0a044fc4deb2491ecce4b0dac7290d33157
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2022-01-03 19:09:25 +0100

    PWM intensity + 1 loop

--- RGB LED/src/main.cpp
+++ RGB LED/src/main.cpp
@@ -12,19 +12,39 @@ void setup()
   pinMode(bluePin, OUTPUT);
 }
 
+int redIntensity = 0;
+
 void loop()
 {
   // put your main code here, to run repeatedly :
-  analogWrite(redPin, 200);
-  delay(500);
-  analogWrite(greenPin, 50);
-  delay(500);
-  analogWrite(bluePin, 25);
-  delay(2000);
-  analogWrite(redPin, 0);
-  delay(500);
-  analogWrite(greenPin, 0);
-  delay(500);
+
+  // set all 3 pins to the desired intensity
+  analogWrite(redPin, redIntensity);
+  analogWrite(greenPin, 255 - redIntensity);
   analogWrite(bluePin, 0);
-  delay(500);
+
+  // remain at this color, but not for very long
+  delay(30);
+
+  // increase the red
+  redIntensity = redIntensity + 1;
+
+  // since 255 is the maximum, set it back to 0
+  // when it increments beyond 255
+  if (redIntensity > 255)
+  {
+    redIntensity = 0;
+  }
+  // analogWrite(redPin, 200);
+  // delay(500);
+  // analogWrite(greenPin, 50);
+  // delay(500);
+  // analogWrite(bluePin, 25);
+  // delay(2000);
+  // analogWrite(redPin, 0);
+  // delay(500);
+  // analogWrite(greenPin, 0);
+  // delay(500);
+  // analogWrite(bluePin, 0);
+  // delay(500);
 }
