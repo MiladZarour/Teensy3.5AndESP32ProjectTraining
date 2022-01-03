0b42456605857339a368294f9e92b4e46ebddda5
Milad Zarour <50852900+MiladZarour@users.noreply.github.com>
2022-01-03 18:22:49 +0100

    added two files, RGB and Blinky-LED

---
+++ RGB LED/TEENSY3.5-and-ESP32-Projects-Training-/src/main.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+
+int redPin = 23;
+int greenPin = 22;
+int bluePin = 21;
+
+void setup()
+{ // put your setup code here, to run once:
+
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  // digitalWrite(redPin, HIGH);
+  // delay(500);
+  // digitalWrite(greenPin, HIGH);
+  // delay(500);
+  // digitalWrite(bluePin, HIGH);
+  // delay(500);
+  // digitalWrite(redPin, LOW);
+  // delay(500);
+  // digitalWrite(greenPin, LOW);
+  // delay(500);
+  // digitalWrite(bluePin, LOW);
+  // delay(500);
+}
